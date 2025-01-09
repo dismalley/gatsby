a8623a5f361358a9a75c77b0716c8b82a26664a5
dismalley <87784181+dismalley@users.noreply.github.com>
2025-01-08 20:29:34 -0600

    Update gatsby-config.js
    url update

--- starters/blog/gatsby-config.js
+++ starters/blog/gatsby-config.js
@@ -15,7 +15,9 @@ module.exports = {
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    siteUrl: `https://ttrpgsite.wpenginepowered.com/`,
+/**    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+*/
     social: {
       twitter: `kylemathews`,
     },
